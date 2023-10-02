871ec1a019738fc8fd8142d0557d87d5e659ca10
Dave Butland <dgbutland@gmail.com>
2023-10-02 18:57:12 +0100

    Fixed provider

--- resources/service_identity/main.tf
+++ resources/service_identity/main.tf
@@ -1,5 +1,5 @@
 resource "google_project_service_identity" "service_identity" {
-  provider = google-uniform_bucket_level_access
+  provider = google-beta
 
   project = var.project
   service = var.service
