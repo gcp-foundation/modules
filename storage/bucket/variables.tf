03da1eda7939ce7dfa5d0b6c60669b4eee9ce6f8
Dave Butland <dgbutland@gmail.com>
2023-10-03 23:58:17 +0100

    Added storage variables

--- storage/bucket/variables.tf
+++ storage/bucket/variables.tf
@@ -7,3 +7,20 @@ variable "location" {
   description = "The location of the storage bucket"
   type        = string
 }
+
+variable "data_classification" {
+  description = "The classification for the data"
+  type        = string
+}
+
+variable "versioning" {
+  description = "Enable versioning"
+  type        = bool
+  default     = true
+}
+
+variable "log_bucket" {
+  description = "Log bucket for logging"
+  type        = string
+  default     = null
+}
