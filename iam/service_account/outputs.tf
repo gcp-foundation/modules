b6fe5c1b025a1602d397f8fcc89dd16e2951ac74
Dave Butland <dgbutland@gmail.com>
2023-10-04 12:24:33 +0100

    Added service account outputs

---
+++ iam/service_account/outputs.tf
@@ -0,0 +1,12 @@
+output "id" {
+  value = google_service_account.service_account.name
+}
+
+output "name" {
+  value = google_service_account.service_account.name
+}
+
+output "email" {
+  value = google_service_account.service_account.email
+}
+
