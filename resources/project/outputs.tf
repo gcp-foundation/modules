b428635cb6f093245568576bc77db0b9bea83fa2
Dave Butland <dgbutland@gmail.com>
2023-10-14 10:47:14 +0100

    fixed outputs

--- resources/project/outputs.tf
+++ resources/project/outputs.tf
@@ -10,6 +10,10 @@ output "number" {
   value = google_project.project.number
 }
 
+output "folder_id" {
+  value = google_project.project.project_id
+}
+
 output "services" {
   value = google_project_service.service
 }
@@ -18,11 +22,3 @@ output "project" {
   value = google_project.project
 }
 
-              + create_time     = "2023-10-05T08:07:18.441Z"
-              + display_name    = "devops"
-              + folder_id       = "223235992948"
-              + id              = "folders/223235992948"
-              + lifecycle_state = "ACTIVE"
-              + name            = "folders/223235992948"
-              + parent          = "organizations/715048305958"
-              + timeouts        = null
