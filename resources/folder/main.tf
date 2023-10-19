c46022b0288dc10cdda3f3c9a2fcf5db27300a19
SudharsaneSivamany <sudharsanes694@gmail.com>
2023-10-19 14:55:23 +0530

    added

--- resources/folder/main.tf
+++ resources/folder/main.tf
@@ -1,23 +1,23 @@
 locals {
   folder_level_1 = [for folder in var.folder_list : folder if folder.parent == null]
-  folder_level_2 = setsubtract(distinct([for folder in var.folder_list : folder.parent == null ? {} : contains(local.folder_level_1.*.display_name, folder.parent) ? folder : {}]), [{}])
-  folder_level_3 = setsubtract(distinct([for folder in var.folder_list : folder.parent == null ? {} : contains(local.folder_level_2.*.display_name, folder.parent) ? folder : {}]), [{}])
+  folder_level_2 = setsubtract(distinct([for folder in var.folder_list : folder.parent == null ? {} : contains(local.folder_level_1.*.displayName, folder.parent) ? folder : {}]), [{}])
+  folder_level_3 = setsubtract(distinct([for folder in var.folder_list : folder.parent == null ? {} : contains(local.folder_level_2.*.displayName, folder.parent) ? folder : {}]), [{}])
 }
 
 resource "google_folder" "folder_level_1" {
-  for_each     = { for folder in local.folder_level_1 : folder.display_name => folder }
-  display_name = each.value.display_name
+  for_each     = { for folder in local.folder_level_1 : folder.displayName => folder }
+  display_name = each.value.displayName
   parent       = var.parent_name
 }
 
 resource "google_folder" "folder_level_2" {
-  for_each     = { for folder in local.folder_level_2 : folder.display_name => folder }
-  display_name = each.value.display_name
+  for_each     = { for folder in local.folder_level_2 : folder.displayName => folder }
+  display_name = each.value.displayName
   parent       = each.value.parent
 }
 
 resource "google_folder" "folder_level_3" {
-  for_each     = { for folder in local.folder_level_3 : folder.display_name => folder }
-  display_name = each.value.display_name
+  for_each     = { for folder in local.folder_level_3 : folder.displayName => folder }
+  display_name = each.value.displayName
   parent       = each.value.parent
 }
