8f7444017e3b9007ad56058cdcac3655f2525b00
SudharsaneSivamany <sudharsanes694@gmail.com>
2023-10-19 12:24:43 +0530

    added output

--- resources/folder/outputs.tf
+++ resources/folder/outputs.tf
@@ -1,5 +1,5 @@
 output "folder_id" {
-  value = merge(flatten([[for folder in local.folder_level_1 : { folder.name = google_folder.folder_level_1[folder.name].name }],
+  value = flatten([[for folder in local.folder_level_1 : { folder.name = google_folder.folder_level_1[folder.name].name }],
     [for folder in local.folder_level_2 : { folder.name = google_folder.folder_level_2[folder.name].name }],
-  [for folder in local.folder_level_3 : { folder.name = google_folder.folder_level_3[folder.name].name }]]))
+  [for folder in local.folder_level_3 : { folder.name = google_folder.folder_level_3[folder.name].name }]])
 }
