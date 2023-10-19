22484a7a2f883cfa0c47aa597edc84845c597f15
SudharsaneSivamany <sudharsanes694@gmail.com>
2023-10-19 15:11:50 +0530

    added

--- resources/folder/outputs.tf
+++ resources/folder/outputs.tf
@@ -1,5 +1,5 @@
 output "folder_id" {
-  value = flatten([[for folder in local.folder_level_1 : { folder.name = google_folder.folder_level_1[folder.name].name }],
-    [for folder in local.folder_level_2 : { folder.name = google_folder.folder_level_2[folder.name].name }],
-  [for folder in local.folder_level_3 : { folder.name = google_folder.folder_level_3[folder.name].name }]])
+  value = flatten([[for folder in local.folder_level_1 : { "${folder.displayName}" = google_folder.folder_level_1[folder.displayName].name }],
+    [for folder in local.folder_level_2 : { "${folder.displayName}" = google_folder.folder_level_2[folder.displayName].name }],
+  [for folder in local.folder_level_3 : { "${folder.displayName}" = google_folder.folder_level_3[folder.displayName].name }]])
 }
