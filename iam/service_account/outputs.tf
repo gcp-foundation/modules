c96e5d80ba7307f8c6ff61feb19b58ce45117c98
Dave Butland <dgbutland@gmail.com>
2023-10-24 13:45:34 +0100

    Added display_name and description to outputs

--- iam/service_account/outputs.tf
+++ iam/service_account/outputs.tf
@@ -10,3 +10,10 @@ output "email" {
   value = google_service_account.service_account.email
 }
 
+output "display_name" {
+  value = google_sercice_account.service_account.display_name
+}
+
+output "description" {
+  value = google_sercice_account.service_account.description
+}
